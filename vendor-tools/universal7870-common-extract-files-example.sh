fdc24723757b31f16fa6c422b7495716a0a8234a
Flominator <github.flominatorgd@gmail.com>
2025-07-07 23:07:18 +0200

    universal7870-common: update universal7870-common-extract-files-example.sh

--- vendor-tools/universal7870-common-extract-files-example.sh
+++ vendor-tools/universal7870-common-extract-files-example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COMMON_A6LTE_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/SAMFW.COM_SM-A600F_AFG_A600FJXU9CVB1_fac"
-COMMON_A6LTE_P_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/Samfw.com_SM-A600F_PAK_A600FJXU9CUH3_fac"
+COMMON_A6LTE_P_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/SAMFW.COM_SM-A600FN_H3G_A600FNXXS5BTC2_fac"
 COMMON_A7Y17LTE_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/Samfw.com_SM-A720S_SKC_A720SKSU5CTL2_fac"
 COMMON_M10LTE_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/Samfw.com_SM-M105F_BKD_M105FDDS4CVB1_fac"
 COMMON_STARLTE_PATH="/media/flominator/40810d67-1e17-42cc-8147-8500cdc3df4b/phoenix_firmware_dumper-main/SAMFW.COM_SM-G960F_DBT_G960FXXUHFVG4_fac"
