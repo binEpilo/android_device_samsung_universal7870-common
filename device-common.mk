36c2d11cadcc9756172a7ee8e888bff8cebcc752
me <lorenz_2828@proton.me>
2026-08-24 23:16:50 +0200

    universal7870-common: Switch to generic AOSP keymaster 4.0
    The Samsung MDFPP keymaster@3.0 crashes due to broken Trustonic
    TEE communication. Switch to the generic AOSP keymaster@4.0
    implementation which works without TEE dependencies.

--- device-common.mk
+++ device-common.mk
@@ -294,15 +294,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
 
 # Keymaster
-# android.hardware.keymaster@3.0-impl.exynos7870 with samsungs MDFPP keystore support
-# NOTICE: works only with trusted devices
-# android.hardware.keymaster@3.0-service.exynos7870 (arm only service)
-#PRODUCT_PACKAGES += \
-#    android.hardware.keymaster@3.0-impl.exynos7870 \
-#    android.hardware.keymaster@3.0-service.exynos7870
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl:32 \
-    android.hardware.keymaster@3.0-service.exynos7870
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
